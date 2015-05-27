2303b7c8ffe96263400f9d3d0026bf09ecd16c42
Sam Hart <hartsn@gmail.com>
2015-05-27 15:03:43 -0400

    add initial files

---
+++ sites_startup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPTS_PATH="/usr/local/bin"
+SCRIPT_PREFIX="site_startup-"
+SCRIPT_SUFFIX=".sh"
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+if [ -f "${SCRIPTS_PATH}/${SCRIPT_PREFIX}${1}${SCRIPT_SUFFIX}" ]; then
+    ${SCRIPTS_PATH}/${SCRIPT_PREFIX}${1}${SCRIPT_SUFFIX}
+else
+    die "Script for $1 not found!"
+fi
+
